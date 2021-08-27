a9164a7d2d65ef49780b4a508d697433a2fea178
Rob Bailiff <robbailiff@gmail.com>
2021-08-27 15:31:09 +0100

    Changed author field in seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -28,7 +28,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
-            author: '6127824b54962eeba11c0ba6',
+            author: process.env.AUTHOR_ID,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
